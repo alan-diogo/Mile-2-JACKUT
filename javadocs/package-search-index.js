e827b273fe6da8c659bf14eb51eb95ad5a08bda9
alan-diogo <alandiogo9@gmail.com>
2025-05-09 03:20:26 -0300

    Jackut - Milestone 2

---
+++ javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.ufal.ic.p2.jackut.model"},{"l":"br.ufal.ic.p2.jackut.model.exceptions"},{"l":"br.ufal.ic.p2.jackut.model.models"}];updateSearchResults();
